b84614470aad5f35f5f69882cdec22c9c1d264ad
Himanshu Kathania <76757042+Himanshu-Kathania@users.noreply.github.com>
2023-11-09 13:40:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const generateMemeBtn = document.querySelector(
+  ".meme-generator .generate-meme-btn"
+);
+const memeImage = document.querySelector(".meme-generator img");
+
+const memeTitle = document.querySelector(" .meme-generator .meme-title");
+const memeAuthor = document.querySelector(" .meme-generator .meme-author");
+
+const updateDetials = (url, title, author) => {
+  memeImage.setAttribute("src", url);
+  memeTitle.innerHTML = title;
+  memeAuthor.innerHTML = author;
+};
+
+const generateMeme = () => {
+  fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((responce) => responce.json())
+    .then((data) => {
+      updateDetials(data.url, data.title, data.author);
+    });
+};
+
+generateMemeBtn.addEventListener("click", generateMeme);
+
+generateMeme();
